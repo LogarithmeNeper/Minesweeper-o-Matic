40c17d0e18e7902326b0aae9e7c8e88e9e384bf9
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2022-01-13 10:54:33 +0100

    Definitions of basic classes for game board creation

---
+++ src/Game.hs
@@ -0,0 +1,25 @@
+
+-- This file presents the modelisation of the game
+
+-- Real value of the tile is what the tile is when generated.
+data RealValue = Mine | Empty 
+    deriving (Eq, Show)
+
+-- Display value of the tile is what the player sees on the boad.
+data DisplayValue = Visible | Invisible | Flag | Unsure
+    deriving (Eq, Show)
+
+-- Coordinates to locate a specific tile
+type Coordinates = (Integer, Integer)
+
+-- This leads us to the following type :
+-- A tile is made of a location, a real value, a display value.
+data Tile = Tile {
+    coordinates :: Coordinates,
+    realValue ::  RealValue,
+    displayValue :: DisplayValue
+}
+    deriving (Eq, Show)
+
+-- The game board will be composed of a 2-dimensional structure of tiles.
+type GameBoard = [[Tile]]
